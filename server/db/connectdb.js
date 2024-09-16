899822668fb859301fef8cb5f32812c0238f6709
Manoj Kumar <87053234+manojkumarydv@users.noreply.github.com>
2024-09-16 12:18:37 +0530

    Update connectdb.js

--- server/db/connectdb.js
+++ server/db/connectdb.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async (DATABASE_URL)=>{
     try {
         const DB_OPTIONS ={
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,
             dbname :'blogdb',
         }
         await mongoose.connect(DATABASE_URL,DB_OPTIONS)
