c211bf1bae51b241bf866fad4cc4b8cf0466cfd3
Manoj Kumar <87053234+manojkumarydv@users.noreply.github.com>
2024-09-16 11:42:49 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import connectDB from './db/connectdb.js'
 const app = express()
 const port = process.env.PORT || '3000'
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/yourdatabase'
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017'
 import web from './routes/web.js'
 
 
